50a1e49388ab7bf38630f0d773b4c46ac830fd27
Annuzzi Daniele <annuzzi.daniele@gmail.com>
2014-01-23 18:01:12 +0100

    Refactor app structure with models

---
+++ WhereAmI/models/Place.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhereAmI.models
+{
+    //Model class has to implement the INotifyPropertyChanged interface
+    //It reflects changes in the data objects
+    //objects are capable of alerting the UI layer of changes to its properties
+    //Note: change will happen on the bound data object itself and not the source list 
+    public class Place : INotifyPropertyChanged
+    {
+
+        private string _name;
+        //Property Definition
+        public string Name
+        {
+            get { return this._name; }
+            //The setter has to call NotifyPropertyChanged to reflect changes
+            set
+            {
+                if (this._name != value)
+                {
+                    this._name = value;
+                    this.NotifyPropertyChanged("Name");
+                }
+            }
+        }
+        private int _cnt;
+        //Property Definition
+        public int Cnt
+        {
+            get { return this._cnt; }
+            //The setter has to call NotifyPropertyChanged to reflect changes
+            set
+            {
+                if (this._cnt != value)
+                {
+                    this._cnt = value;
+                    this.NotifyPropertyChanged("Cnt");
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void NotifyPropertyChanged(string propName)
+        {
+            if (this.PropertyChanged != null)
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propName));
+        }
+
+        public Place(string name, List<Wifi> wifis)
+        {
+            this.Name = name;
+            this.Wifis = wifis;
+            this.Cnt = 0;
+        }
+
+        public List<Wifi> Wifis { get; set; }
+        public List<Action> BeforeActions { get; set; }
+        public List<Action> InActions { get; set; }
+        public List<Action> AfterActions { get; set; }
+
+        public override string ToString()
+        {
+            return this.Name;
+        }
+    }
+}
---
+++ WhereAmI/models/Wifi.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhereAmI.models
+{
+    public class Wifi
+    {
+        public string SSID { get; set; }
+
+        public float Power { get; set; }
+        public int PowerPerc { get; set; }
+
+        public override string ToString()
+        {
+            return this.SSID + ": " + this.PowerPerc;
+        }
+    }
+}
