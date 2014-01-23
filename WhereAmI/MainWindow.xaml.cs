1f2393dc513d3a779517b7c60e7b07d541fc4bff
Annuzzi Daniele <annuzzi.daniele@gmail.com>
2014-01-23 18:10:22 +0100

    Refactor bind/load data and refresh logic (always with mock data)

--- WhereAmI/MainWindow.xaml.cs
+++ WhereAmI/MainWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WhereAmI.models;
 
 namespace WhereAmI
 {
@@ -24,9 +25,9 @@ namespace WhereAmI
     {
         class CurrentState : INotifyPropertyChanged
         {
-            private PlaceModel _place;
+            private Place _place;
             //Property Definition
-            public PlaceModel Place
+            public Place Place
             {
                 get { return this._place; }
                 //The setter has to call NotifyPropertyChanged to reflect changes
@@ -53,156 +54,117 @@ namespace WhereAmI
         //A list that notifies any destinations of changes to its content
         //It works only when an item is added or deleted
         //It reflects changes in the list data source
-        private ObservableCollection<PlaceModel> places = new ObservableCollection<PlaceModel>();
-        private ObservableCollection<WifiModel> wifis = new ObservableCollection<WifiModel>();
+        private ObservableCollection<Place> places = new ObservableCollection<Place>();
+        private ObservableCollection<Wifi> wifis = new ObservableCollection<Wifi>();
+
+        private void loadData()
+        {
+            //Mock for snapshot 
+            List<Wifi> snapshot1 = new List<Wifi>();
+            snapshot1.Add(new Wifi() { SSID = "Home", PowerPerc = 100 });
+            snapshot1.Add(new Wifi() { SSID = "HomeDani", PowerPerc = 1 });
+            snapshot1.Add(new Wifi() { SSID = "dlink", PowerPerc = 10 });
+
+            List<Wifi> snapshot2 = new List<Wifi>();
+            snapshot2.Add(new Wifi() { SSID = "Polito", PowerPerc = 10 });
+            snapshot2.Add(new Wifi() { SSID = "Lab1", PowerPerc = 30 });
+            snapshot2.Add(new Wifi() { SSID = "Lab2", PowerPerc = 50 });
+
+            List<Wifi> snapshot3 = new List<Wifi>();
+            snapshot3.Add(new Wifi() { SSID = "CompanyHall", PowerPerc = 50 });
+            snapshot3.Add(new Wifi() { SSID = "CompanyRoom1", PowerPerc = 20 });
+            snapshot3.Add(new Wifi() { SSID = "CompanyLab", PowerPerc = 80 });
+
+            //Mock for places 
+            Place p1 = new Place("Home", snapshot1);
+            Place p2 = new Place("Polito", snapshot2);
+            Place p3 = new Place("Work", snapshot3);
+
+            places.Add(p1);
+            places.Add(p2);
+            places.Add(p3);  
+        }
+
+        private void bindData()
+        {   
+            //Bind the source data for the template View
+            wifiData.ItemsSource = wifis;
+            placesViewData.ItemsSource = places;
+            vState.DataContext = currentState;
+        }
 
         public MainWindow()
         {
             InitializeComponent();
-
-            //Mock for wifis
-            for (var i = 0; i < 10; i++)
-            {
-                wifis.Add(new WifiModel() { SSID = "AAAAAA", PowerPerc = 42 });
-                wifis.Add(new WifiModel() { SSID = "BBB", PowerPerc = 39 });
-                wifis.Add(new WifiModel() { SSID = "CCCCCCCCC", PowerPerc = 13 });
-            }
-            wifiData.ItemsSource = wifis;
-
-            //Mock for snapshot 
-            List<WifiModel> snapshot1 = new List<WifiModel>();
-            snapshot1.Add(new WifiModel() { SSID = "AAA", PowerPerc = 100 });
-            snapshot1.Add(new WifiModel() { SSID = "BBB", PowerPerc = 1 });
-            snapshot1.Add(new WifiModel() { SSID = "CCCCCC", PowerPerc = 10 });
-
-            List<WifiModel> snapshot2 = new List<WifiModel>();
-            snapshot2.Add(new WifiModel() { SSID = "AAA", PowerPerc = 10 });
-            snapshot2.Add(new WifiModel() { SSID = "BBB", PowerPerc = 30 });
-            snapshot2.Add(new WifiModel() { SSID = "CCCCCCCCC", PowerPerc = 50 });
-
-            List<WifiModel> snapshot3 = new List<WifiModel>();
-            snapshot3.Add(new WifiModel() { SSID = "A", PowerPerc = 50 });
-            snapshot3.Add(new WifiModel() { SSID = "BBB", PowerPerc = 20 });
-            snapshot3.Add(new WifiModel() { SSID = "CCC", PowerPerc = 80 });
-
-            //Mock for places 
-            PlaceModel p1 = new PlaceModel("Casa", snapshot1);
-            PlaceModel p2 = new PlaceModel("Polito", snapshot2);
-            PlaceModel p3 = new PlaceModel("Lavoro", snapshot3);
-
-
-            places.Add(p1);
-            places.Add(p2);
-            places.Add(p3);
-            //Set the source data for the Template View
-            placesViewData.ItemsSource = places;
-
-
-            currentState.Place = p1;
-            vState.DataContext = currentState;
-        }
-
-        public class WifiModel
-        {
-            public string SSID { get; set; }
-
-            public float Power { get; set; }
-            public int PowerPerc { get; set; }
-
-            public override string ToString()
-            {
-                return this.SSID + ": " + this.PowerPerc;
-            }
-        }
-
-        //Model class has to implement the INotifyPropertyChanged interface
-        //It reflects changes in the data objects
-        //objects are capable of alerting the UI layer of changes to its properties
-        //Note: change will happen on the bound data object itself and not the source list 
-        public class PlaceModel : INotifyPropertyChanged
-        {
-
-            private string _name;
-            //Property Definition
-            public string Name
-            {
-                get { return this._name; }
-                //The setter has to call NotifyPropertyChanged to reflect changes
-                set
-                {
-                    if (this._name != value)
-                    {
-                        this._name = value;
-                        this.NotifyPropertyChanged("Name");
-                    }
-                }
-            }
-
-            public event PropertyChangedEventHandler PropertyChanged;
-
-            public void NotifyPropertyChanged(string propName)
-            {
-                if (this.PropertyChanged != null)
-                    this.PropertyChanged(this, new PropertyChangedEventArgs(propName));
-            }
-
-            public PlaceModel(string name, List<WifiModel> wifis)
-            {
-                this.Name = name;
-                this.Wifis = wifis;
-                this.Cnt = 0;
-            }
-
-            public int Cnt { get; set; }
-            public List<WifiModel> Wifis { get; set; }
-            public List<ActionModel> BeforeActions { get; set; }
-            public List<ActionModel> InActions { get; set; }
-            public List<ActionModel> AfterActions { get; set; }
-
-            public override string ToString()
-            {
-                return this.Name;
-            }
-        }
-
-        public class ActionModel
-        {
-            public string Name { get; set; }
+            loadData();
+            bindData();
         }
 
         private void btnChangePlace_Click(object sender, RoutedEventArgs e)
         {
             //cast of object to a type with as
             if (placesViewData.SelectedItem != null)
-                (placesViewData.SelectedItem as PlaceModel).Name = "Random Name";
+                (placesViewData.SelectedItem as Place).Name = "Random Name";
         }
 
         private void btnDeletePlace_Click(object sender, RoutedEventArgs e)
         {
             if (placesViewData.SelectedItem != null)
-                places.Remove(placesViewData.SelectedItem as PlaceModel);
+            {
+                places.Remove(placesViewData.SelectedItem as Place);
+            }
         }
 
         private void btnAddPlace_Click(object sender, RoutedEventArgs e)
         {
             Random random = new Random();
             int randomNumber = random.Next(0, 30);
-            places.Add(new PlaceModel(randomNumber+"FFF" , wifis.ToList<WifiModel>()));
+            places.Add(new Place(randomNumber+"FFF" , wifis.ToList<Wifi>()));
         }
 
+        private void btnResetStatPlace_Click(object sender, RoutedEventArgs e)
+        {
+            Place p = placesViewData.SelectedItem as Place;
+            p.Cnt += 10;
+        }
+
+        //Refresh the system from UI manually
+        //TODO 
+        //refresh periodically
         private void btnRefresh_Click(object sender, RoutedEventArgs e)
+        {
+            readCurrentWifis();
+            computeCurrentPlace();
+        }
+
+        //Retrieve the current place from the list of recorded places
+        //using the triangulation algorithm 
+        //that receives in input the list of current detected wifis
+        private void computeCurrentPlace()
+        {
+            Random random = new Random();
+            int randomNumber = random.Next(0, 3);
+            currentState.Place = places.ElementAt(random.Next(0, places.Count));
+            //TODO
+            //currentState.Place = triangAlgo(places, wifis);
+        }
+
+        //Read the current wifis networks connections from the WLAN API
+        //update the wifis list
+        private void readCurrentWifis()
         {
             wifis.Clear();
             Random random = new Random();
             int randomNumber = random.Next(0, 3);
             for (var i = 0; i < randomNumber; i++)
             {
-                wifis.Add(new WifiModel() { SSID = "AAAAAA", PowerPerc = random.Next(0, 100)});
-                wifis.Add(new WifiModel() { SSID = "BBB", PowerPerc = random.Next(0, 100) });
-                wifis.Add(new WifiModel() { SSID = "CCCCCCCCC", PowerPerc = random.Next(0, 100) });
+                wifis.Add(new Wifi() { SSID = "AAAAAA", PowerPerc = random.Next(0, 100) });
+                wifis.Add(new Wifi() { SSID = "BBB", PowerPerc = random.Next(0, 100) });
+                wifis.Add(new Wifi() { SSID = "CCCCCCCCC", PowerPerc = random.Next(0, 100) });
             }
-
-            currentState.Place = places.ElementAt(random.Next(0, places.Count));
+            //TODO
+            //create wifi models for wifis list using WLAN API
         }
+
     }
 }
