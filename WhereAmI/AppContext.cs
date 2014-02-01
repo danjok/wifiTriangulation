f5c8ba050c7e21971ca2fdd5f4d239769cafdfd2
Annuzzi Daniele <annuzzi.daniele@gmail.com>
2014-02-01 16:52:50 +0100

    Add Actions (tab, list view, data in db)

--- WhereAmI/AppContext.cs
+++ WhereAmI/AppContext.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Data.Entity;
 using System.Linq;
 using System.Text;
@@ -9,12 +10,12 @@ using WhereAmI.models;
 namespace WhereAmI
 {
     //create your custom DB initializer by inheriting one of the intializer 
-    public class AppContextInitializer : CreateDatabaseIfNotExists<AppContext>
+    public class AppContextInitializer : DropCreateDatabaseAlways<AppContext>
     {
         protected override void Seed(AppContext context)
         {
             IList<Place> defaultPlaces = new List<Place>();
-            
+
             List<Wifi> snapshot1 = new List<Wifi>();
             snapshot1.Add(new Wifi() { SSID = "Home", PowerPerc = 100 });
             snapshot1.Add(new Wifi() { SSID = "HomeDani", PowerPerc = 1 });
@@ -30,13 +31,30 @@ namespace WhereAmI
             snapshot3.Add(new Wifi() { SSID = "CompanyRoom1", PowerPerc = 20 });
             snapshot3.Add(new Wifi() { SSID = "CompanyLab", PowerPerc = 80 });
 
-            defaultPlaces.Add(new Place() { Name = "Home", Cnt = 0, Snapshot = Place.serializationSnapshot(snapshot1) });
-            defaultPlaces.Add(new Place() { Name = "Polito", Cnt = 0, Snapshot = Place.serializationSnapshot(snapshot2) });
-            defaultPlaces.Add(new Place() { Name = "Work", Cnt = 0, Snapshot = Place.serializationSnapshot(snapshot3) });
+            //Actions
+            models.Action a1 = new models.Action() { ActionId = 1, Name = "Activate Audio", Command = "the string command 1 " };
+            models.Action a2 = new models.Action() { ActionId = 2, Name = "Increase Brightness", Command = "the string command 2" };
+            models.Action a3 = new models.Action() { ActionId = 3, Name = "Dummy command", Command = "the string command 3" };
+
+            context.Actions.Add(a1);
+            context.Actions.Add(a2);
+            context.Actions.Add(a3);
+
+            ObservableCollection<models.Action> actions1 = new ObservableCollection<models.Action>();
+            actions1.Add(a1);
+            actions1.Add(a2);
+
+            ObservableCollection<models.Action> actions2 = new ObservableCollection<models.Action>();
+            actions2.Add(a3);
+            actions2.Add(a2);
+
+            defaultPlaces.Add(new Place() { Name = "Home", Cnt = 0, Snapshot = Place.serializationSnapshot(snapshot1), InActions = actions1 });
+            defaultPlaces.Add(new Place() { Name = "Polito", Cnt = 0, Snapshot = Place.serializationSnapshot(snapshot2), InActions = null });
+            defaultPlaces.Add(new Place() { Name = "Work", Cnt = 0, Snapshot = Place.serializationSnapshot(snapshot3), InActions = actions2 });
 
             foreach (Place p in defaultPlaces)
                 context.Places.Add(p);
-            
+
             //All standards will
             base.Seed(context);
         }
@@ -45,7 +63,8 @@ namespace WhereAmI
     public class AppContext : DbContext
     {
         //public AppContext(): base()
-        public AppContext(): base("WhereAmI_DBConnectionString")
+        public AppContext()
+            : base("WhereAmI_DBConnectionString")
         {
             //Database.SetInitializer<AppContext>(new CreateDatabaseIfNotExists<AppContext>());
             //Database.SetInitializer<AppContext>(new DropCreateDatabaseAlways<AppContext>());
@@ -56,5 +75,5 @@ namespace WhereAmI
         public DbSet<Place> Places { get; set; }
         public DbSet<WhereAmI.models.Action> Actions { get; set; }
     }
-   
+
 }
---
+++ WhereAmI/views/ActionsView.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WhereAmI.models;
+using System.Data.Entity;
+
+namespace WhereAmI.views
+{
+    /// <summary>
+    /// Interaction logic for ActionsView.xaml
+    /// </summary>
+    public partial class ActionsView : UserControl
+    {
+        public ActionsView()
+        {
+            InitializeComponent();
+        }
+
+        private void OnControlLoaded(object sender, RoutedEventArgs e)
+        {
+            //placesViewData.ItemsSource = DataManager.Instance.places;
+
+            // Load is an extension method on IQueryable,  
+            // defined in the System.Data.Entity namespace. 
+            // This method enumerates the results of the query,  
+            // similar to ToList but without creating a list. 
+            // When used with Linq to Entities this method  
+            // creates entity objects and adds them to the context.
+            var ctx = DataManager.Instance.context;
+            ctx.Actions.Load();
+
+            // After the data is loaded call the DbSet<T>.Local property  
+            // to use the DbSet<T> as a binding source. 
+            actionsViewData.ItemsSource = ctx.Actions.Local;
+        }
+    }
+}
