041b74d3f36ecd50086430d80934d09109fba8ba
Annuzzi Daniele <annuzzi.daniele@gmail.com>
2014-02-18 17:24:18 +0100

    BugFix

--- WhereAmI/AppContext.cs
+++ WhereAmI/AppContext.cs
@@ -10,7 +10,7 @@ using WhereAmI.models;
 namespace WhereAmI
 {
     //create your custom DB initializer by inheriting one of the intializer
-    public class AppContextInitializer : DropCreateDatabaseIfModelChanges<AppContext>
+    public class AppContextInitializer : CreateDatabaseIfNotExists<AppContext>
     {
         protected override void Seed(AppContext context)
         {
